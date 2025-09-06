c0dd42f754b0b76fec94f8dbfcf8939e73af10bf
itachiszep <itachiszep@int.pl>
2025-09-06 15:44:11 +0200

    added pocotafera

--- src/pages/muzyka/typeg/pocotafera.jsx
+++ src/pages/muzyka/typeg/pocotafera.jsx
@@ -5,6 +5,7 @@ const pocotaafera = () => {
  <div>
       <div className='center'>
         <img className="itachi" src="/images/itachi-uchiha-naruto-amoled-black-background-minimal-art-3840x2160-6478.jpg"/>
+<iframe width="853" height="480" src="https://www.youtube.com/embed/GoVaAB6QBtc"></iframe>
 <iframe width="853" height="480" src="https://www.youtube.com/embed/6PCvhNPWRsE"></iframe> </div>
     </div>  )
 }
