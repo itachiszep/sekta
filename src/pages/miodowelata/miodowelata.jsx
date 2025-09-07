b6ea482b40209e977a51e968a273d9e42de7f1d9
itachiszep <itachiszep@int.pl>
2025-09-07 17:39:25 +0200

    added miodowelata

---
+++ src/pages/miodowelata/miodowelata.jsx
@@ -0,0 +1,174 @@
+import React from 'react'
+
+const miodowelata = () => {
+  return (
+ <div>
+      <div className='center'>
+        <img className="itachi" src="/images/itachi-uchiha-naruto-amoled-black-background-minimal-art-3840x2160-6478.jpg"/>
+        <h3>SEZON 1</h3>
+<ul>
+  <li>1 - Żywe zwłoki - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/24/24frru2xvk28wh3kkx1xongg4boar6va.mp4">link</a></li>
+  <li>2 - List do szefa - <a href="https://ipla-e2-17.pluscdn.pl/p/vm2movies/b1/b1wpucgqtf7yqtm59tsoou4cx4v9qh17.mp4">link</a></li>
+  <li>3 - Głowa rodziny - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/a4/a45g7xmwbe6sdho6rz61eydp6a8pig4d.mp4">link</a></li>
+  <li>4 - Kucharz przyszłości - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/qb/qbmkg8b9guayd4h5oi1yiytfdqtcvxa2.mp4">link</a></li>
+  <li>5 - Kto PIT-a, nie błądzi - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/e3/e3in4nkrxtdi53959y5czhqg9k1go5oq.mp4">link</a></li>
+  <li>6 - Witaj mamo - <a href="https://ipla-e2-20.pluscdn.pl/p/vm2movies/v1/v1qe975ncno3ti1xgiomfy5h29pm8egy.mp4">link</a></li>
+  <li>7 - Robot kuchenny - <a href="https://ipla-e1-79.pluscdn.pl/p/vm2movies/i8/i82xgtyko2jt4ywa5t7tgh2a6mwitub7.mp4">link</a></li>
+  <li>8 - Mocne plecy - <a href="https://ipla-e1-79.pluscdn.pl/p/vm2movies/ck/ckorjwe29xja32h581g6yj7ciz247zgg.mp4">link</a></li>
+  <li>9 - Golfista - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/kh/khq3a68vjubc7rxqdt25fgq7xgdq3hnv.mp4">link</a></li>
+  <li>10 - Ptak na parapecie - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/ja/ja6mw7n5v75yoz9iatb3trj1meuy12og.mp4">link</a></li>
+  <li>11 - Na nowej drodze życia - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/6i/6ixz3n7qp21w59tbutscw1k5uhjyv4a1.mp4">link</a></li>
+  <li>12 - Karol Krawczyk S.A - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/5x/5xy4rbvc86ahwmxg25k4iugb3cbz8qf9.mp4">link</a></li>
+  <li>13 - Przed pierwszą gwiazdką - <a href="https://ipla-e2-20.pluscdn.pl/p/vm2movies/j9/j9utr4ujq7t11ftm218vtghyyf2kjxsk.mp4">link</a></li>
+</ul>
+
+<h3>SEZON 2</h3>
+<ul>
+  <li>14 - Niespodzianka urodzinowa - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/t3/t3tdteyais5hc4q456byjfhebv8b8dfv.mp4">link</a></li>
+  <li>15 - Awans - <a href="https://ipla-e1-83.pluscdn.pl/p/vm2movies/11/11tyirnffdpvuu7m8v7zgyeq9h97h1dr.mp4">link</a></li>
+  <li>16 - Sierżant roku - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/zw/zwaxi3bmmdpdr58qvgzwgj5onpns6sht.mp4">link</a></li>
+  <li>17 - Pieskie życie - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/yc/ycobi3ykmnpykze244ycbdu9kqrtcucs.mp4">link</a></li>
+  <li>18 - Lewa forsa - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/iu/iu6whsap3ydjztd9dg3mfa3jj9e257q1.mp4">link</a></li>
+  <li>19 - Eksmisja - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/bc/bcsij538b8w9ej596ix4zfke2qth6abn.mp4">link</a></li>
+  <li>20 - Odpowiedź za 99.000 zł. - <a href="https://ipla-e2-18.pluscdn.pl/p/vm2movies/qs/qs9ms253q6oefyfhy4251ve3en32gpcn.mp4">link</a></li>
+  <li>21 - Mój braciszek Karol - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/q8/q8698ne82z4zbxrh9jeidizi1ngktiyv.mp4">link</a></li>
+  <li>22 - Lunatyk - <a href="https://ipla-e2-20.pluscdn.pl/p/vm2movies/fk/fk8b9854mj6n124xj5n3pwzs69z4436p.mp4">link</a></li>
+  <li>23 - Smak wolności - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/k9/k9vx19z2z1om7m6wqdvg45a6974mf6y1.mp4">link</a></li>
+  <li>24 - Przybysz z kosmosu - <a href="https://ipla-e2-20.pluscdn.pl/p/vm2movies/s7/s7z6qitnp2766fms76vho4iexfdjw8q5.mp4">link</a></li>
+  <li>25 - Wspaniały kumpel - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/ju/junu1je1ke6k3jvma63tzftx2hva65ew.mp4">link</a></li>
+  <li>26 - Magia sceny - <a href="https://ipla-e1-79.pluscdn.pl/p/vm2movies/rk/rk2f84thfvp7ym26s2vb16tjinjyvdy2.mp4">link</a></li>
+</ul>
+
+<h3>SEZON 3</h3>
+<ul>
+  <li>27 - Decydujący głos - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/23/239wyd5yib3y1n2tqdxdvsxwor7bu63w.mp4">link</a></li>
+  <li>28 - TV or not TV - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/8o/8o1y94cdy6e2q42ax781zg2nx2myeqc2.mp4">link</a></li>
+  <li>29 - Odmienny stan - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/xv/xv1xdfh62fbgu5cnvnmree46hnhbhrbi.mp4">link</a></li>
+  <li>30 - Męska duma - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/cv/cv7pdoiufim439ha27fvgjpoybawd31z.mp4">link</a></li>
+  <li>31 - Dzikość serca - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/93/93usw4exohsowbj9ie1hzsqbdr7oqj9y.mp4">link</a></li>
+  <li>32 - Opiekunka do dziecka - <a href="https://redirector.redefine.pl/vm2movies/685v2haook6ghuhobqhmxcyd1rg9i13p.mp4">link</a></li>
+  <li>33 - Duży miś - <a href="https://ipla-e2-18.pluscdn.pl/p/vm2movies/ob/obe1jjimdb2mf43xk7h59yqx5m3eibbj.mp4">link</a></li>
+  <li>34 - Wielka szansa - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/by/byr34xaozbogsgfozs2mgo5wcw41xwby.mp4">link</a></li>
+  <li>35 - Na ryby - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/8k/8k14fovb48nibco78gwib6j44n7afgr7.mp4">link</a></li>
+  <li>36 - Fatum Los Przeznaczenie - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/yj/yj412czaazhjwiwocw1kqhbby5wgwock.mp4">link</a></li>
+  <li>37 - Sprawa wielkiej wagi - <a href="https://ipla-e2-17.pluscdn.pl/p/vm2movies/z1/z1eg7bkvpz34j7xu2rn5ey7yp9zo5fay.mp4">link</a></li>
+  <li>38 - Alina i Blondyna - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/kj/kjz534jf8bumuow9o8vw2ha6jzvo3os4.mp4">link</a></li>
+  <li>39 - Wiecznie młodzi - <a href="https://ipla-e1-79.pluscdn.pl/p/vm2movies/ch/chmbcmf9qpv9fdpnkkuex71rpz324ory.mp4">link</a></li>
+  <li>40 - Pieśń strudzonego renifera [odcinek specjalny] - <a href="https://ipla-e2-19.pluscdn.pl/p/vm2movies/76/76oik4f27fjpms4ts52sm12bwyecuf61.mp4">link</a></li>
+</ul>
+<h3>SEZON 4</h3>
+<ul>
+  <li>41 - Na psa urok - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/8p/8p7pu6zvf1yf4ngc9mc8kjp5epfg1k1a.mp4">link</a></li>
+  <li>42 - Męski sport - <a href="https://ipla-e2-19.pluscdn.pl/p/vm2movies/y1/y1omjf3ehknc8hrgkwbdbs6e5j1g19sm.mp4">link</a></li>
+  <li>43 - Malwina - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/4b/4bifc9d1qysbdkp4f8j9py6xdjj6zm3x.mp4">link</a></li>
+  <li>44 - Karol i Karol - <a href="https://ipla-e1-79.pluscdn.pl/p/vm2movies/f3/f3o87w77tnm8nybpufm2c9sxn66yr11z.mp4">link</a></li>
+  <li>45 - Kraina miłości - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/tf/tfcz7k6sy93zmpysofgtrtjywpd5vh87.mp4">link</a></li>
+  <li>46 - Gołąbki z mięskiem - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/fu/fu3pr1qmn2to8yhsnue2ra1gsy8d9bno.mp4">link</a></li>
+  <li>47 - 500 kilo - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/pz/pzgv8r7vtr1bwvsoevyp9z6j8o6efqft.mp4">link</a></li>
+  <li>48 - Dziennikarze - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/1o/1oh8wz2c3xj1qdybuw5bcip85yk1qwhb.mp4">link</a></li>
+  <li>49 - Krótkie spięcie - <a href="https://ipla-e2-19.pluscdn.pl/p/vm2movies/4k/4klfjg0jsrtqec98v7zsnl6efxs8m2f5.mp4">link</a></li>
+  <li>50 - Samotni - <a href="https://ipla-e1-79.pluscdn.pl/p/vm2movies/fd/fd3p8h4m5y9fnnb8zmn6s7xnjkmvnw0a.mp4">link</a></li>
+  <li>51 - Ciemna strona mocy - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/r6/r6l48j1rpq1hz62tsvwikgk5ej3trh2d.mp4">link</a></li>
+  <li>52 - Praca dla wszystkich - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/25/25s8pum77jzpsps3g4e2yz5kbqgkydng.mp4">link</a></li>
+  <li>53 - Ślubu nie będzie - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/2y/2ygtj4yef92fvy9h38mmex7m6txmkjgr.mp4">link</a></li>
+  <li>54 - Krawczyk i Syn - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/s7/s7e1uh7gafm7st3k29whdfx6usbs5zdm.mp4">link</a></li>
+</ul>
+
+<h3>SEZON 5</h3>
+<ul>
+  <li>55 - Między nami lokatorami - <a href="https://ipla-e2-19.pluscdn.pl/p/vm2movies/rs/rsos3k1hxfrdwrbkngbbo4hzk1a4gjp4.mp4">link</a></li>
+  <li>56 - Akcja Integracja - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/ju/juu7hs2nmmavh35qz1bm3pi6ndqsmrbj.mp4">link</a></li>
+  <li>57 - Kto pod kim dołki kopie - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/vs/vsv1cqv43c9ttxyd0meqsyeb8ytb3ydr.mp4">link</a></li>
+  <li>58 - Lusia i Malinowscy - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/6o/6o1gdhfuhm6xn5q6p82kgmq3ng1w3i5p.mp4">link</a></li>
+  <li>59 - Śpij, kochany, śpij - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/au/aumeyh5fp7ufb6hd4fh0p5s0qteqeqyw.mp4">link</a></li>
+  <li>60 - To nie wypanda - <a href="https://ipla-e1-83.pluscdn.pl/p/vm2movies/1b/1bxvtgi5r1a2z9bdkj6j1uw7fuhdw14n.mp4">link</a></li>
+  <li>61 - Pieniądze szczęścia nie dają - <a href="https://ipla-e2-18.pluscdn.pl/p/vm2movies/sg/sgahg3r6uy3q62tk07w6of5i9rrwm1kn.mp4">link</a></li>
+  <li>62 - Kurs samoobrony - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/gm/gm55y6rcv2p23jq2ey1sfrnpkpjyx96k.mp4">link</a></li>
+  <li>63 - Afera kryminalna - <a href="https://ipla-e2-18.pluscdn.pl/p/vm2movies/mf/mfeyj41ebdwt9mkqd1l0c65u3pxfsq4z.mp4">link</a></li>
+  <li>64 - Interesy - <a href="https://ipla-e2-19.pluscdn.pl/p/vm2movies/w7/w72rfgr3f32h6uyxzbhpxmj6a4i3ggxo.mp4">link</a></li>
+  <li>65 - Wesołych Świąt - <a href="https://ipla-e2-17.pluscdn.pl/p/vm2movies/so/sofr9tuwnjnb7sqrsww9ecr1xxctnp4g.mp4">link</a></li>
+  <li>66 - Miłość w pracy - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/2m/2m3gkxpgcz31vckniws4c1ioehlmq3ef.mp4">link</a></li>
+  <li>67 - Miłość, pieniądze, złudzenia - <a href="https://ipla-e2-19.pluscdn.pl/p/vm2movies/oe/oe7r2m7tyqog3o2oufqkqhsnacx35zft.mp4">link</a></li>
+  <li>68 - Złota rybka - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/fx/fxwjyqjbn4mnqgqnjr3h9e8dhr1iyj9w.mp4">link</a></li>
+</ul>
+<h3>SEZON 6</h3>
+<ul>
+  <li>69 - Choroba - <a href="https://ipla-e1-83.pluscdn.pl/p/vm2movies/6j/6jgyw7gsczst7qgxniyye3jlt1y9nb5p.mp4">link</a></li>
+  <li>70 - Tata - <a href="https://ipla-e1-83.pluscdn.pl/p/vm2movies/nw/nwos9jkt9f17m93ab32mymrkqfeyjvgh.mp4">link</a></li>
+  <li>71 - Wnuczek - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/3d/3dz11xj1rn1qrb4fi4ut9whwtk79hcz4.mp4">link</a></li>
+  <li>72 - Młodszy sierżant - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/hp/hp97tx0c4cdkzq51gzt7yckg1rh97o4u.mp4">link</a></li>
+  <li>73 - Powrót - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/sg/sgl8xx2r23g5hffmy9j6zm5slw3nb27q.mp4">link</a></li>
+  <li>74 - Kontrakt - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/7u/7u3u3pcjqn5pjb8psxd5yk5dg6syutio.mp4">link</a></li>
+  <li>75 - Fotografia - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/h6/h65j7ny6qniw7f6iyb1h4x7qx67so3ae.mp4">link</a></li>
+  <li>76 - Dorosłość - <a href="https://ipla-e2-18.pluscdn.pl/p/vm2movies/r3/r3nn9e7h4kyg5s8t9uzchqcb53z9yb1j.mp4">link</a></li>
+  <li>77 - Wypadek - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/7a/7a7mpzpdzk6bg9mbpdpu7hcfhqqjq3ms.mp4">link</a></li>
+  <li>78 - Nowy sąsiad - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/vm/vm3dxn8i6n93p3b4u2vhr8jrn5w9f7xw.mp4">link</a></li>
+  <li>79 - Malarz pokojowy - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/uz/uzcs9p7h9ur5o3y3ffhcg1d1nc7jtqkn.mp4">link</a></li>
+  <li>80 - Dzień Kobiet - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/6b/6bbsm4mf4egxx8o6iaybj3ykqos42yhh.mp4">link</a></li>
+  <li>81 - Wywiad - <a href="https://ipla-e1-79.pluscdn.pl/p/vm2movies/v1/v1v5a7qf1yod6fs92wjm7gyb8p6nrs11.mp4">link</a></li>
+  <li>82 - Przeprowadzka - <a href="https://ipla-e2-17.pluscdn.pl/p/vm2movies/9u/9u5ydoa94f6qqj59xqegqg9yr1wjky1f.mp4">link</a></li>
+</ul>
+
+<h3>SEZON 7</h3>
+<ul>
+  <li>83 - Matka boska z Sasykiem - <a href="https://ipla-e1-79.pluscdn.pl/p/vm2movies/2c/2c5iv9sfvzxyqg4j4d6frz4bhk71xzga.mp4">link</a></li>
+  <li>84 - Internet - <a href="https://ipla-e2-20.pluscdn.pl/p/vm2movies/lt/lt6eb42r9rpp4wjnz61kk3yd7gyx6fqu.mp4">link</a></li>
+  <li>85 - Wigilijny konkurs - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/3f/3f6bi1rfyabdn81i3z3ko8quzrdr2xsj.mp4">link</a></li>
+  <li>86 - Kurs komputerowy - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/sf/sf36v5rjyrpmkn4d26db72ge89tq1f39.mp4">link</a></li>
+  <li>87 - Tancerz - <a href="https://ipla-e2-18.pluscdn.pl/p/vm2movies/5m/5mr8mib2zyz3m5dpnrqt4k5h7sbk7xvq.mp4">link</a></li>
+  <li>88 - Obywatel świata - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/ho/ho4giocaz3zfd64uhr3kzcy7o5q7a3jv.mp4">link</a></li>
+  <li>89 - Harcerz - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/fq/fq6ddn6yixx4jcm4nw3smchb5gk3yk39.mp4">link</a></li>
+  <li>90 - Urodziny Karola - <a href="https://ipla-e2-19.pluscdn.pl/p/vm2movies/p6/p6bwoy78huhwz3sd7k3wsnzht5n73fqu.mp4">link</a></li>
+  <li>91 - Artyści - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/qj/qjv8mbqvm6bs9kcx7sygwxxujpyuvixb.mp4">link</a></li>
+  <li>92 - Kapelusz - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/fp/fp23nb3os3rfhv3p58bq65pwq9bf61s2.mp4">link</a></li>
+  <li>93 - Mechanik - <a href="https://ipla-e1-76.pluscdn.pl/p/vm2movies/q8/q8cfo2xug84bb3wqxwh7ntufsgfwe51x.mp4">link</a></li>
+  <li>94 - Egzamin - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/og/og6bn4heax59zh6r6xshoa17d5eyczpw.mp4">link</a></li>
+  <li>95 - Długi weekend - <a href="https://ipla-e2-20.pluscdn.pl/p/vm2movies/y8/y86rr7npz8of53dqz9fr71jfrc4i6f5r.mp4">link</a></li>
+  <li>96 - Nowe pokolenie - <a href="https://ipla-e2-17.pluscdn.pl/p/vm2movies/9o/9o7gg3th8cz2wb15qzvd2n5w5ecgmrbi.mp4">link</a></li>
+</ul>
+
+<h3>SEZON 8</h3>
+<ul>
+  <li>97 - Karol na emeryturze - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/fx/fx3tq2hbkz2t1c4o8w2d7v1m9f1j1qoy.mp4">link</a></li>
+  <li>98 - Emeryt - <a href="https://ipla-e2-17.pluscdn.pl/p/vm2movies/c3/c3z4dpbyc3v3rbryc6imycwm7gsgc1yt.mp4">link</a></li>
+  <li>99 - Ojciec dyrektor - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/gf/gf6xjizk7dsh5mnmj1w6bc8qge8ihq3n.mp4">link</a></li>
+  <li>100 - Urlop - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/tj/tjbq3r9a1vbexg4d7mivx5fmc9qav9j2.mp4">link</a></li>
+  <li>101 - Teściowa - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/9p/9p5o74x1vq33kjk9i7veb6o8c7cnos8a.mp4">link</a></li>
+  <li>102 - Dzień dziecka - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/cy/cy55eqecjs9r7kb56ch1drz5crpmbk9k.mp4">link</a></li>
+  <li>103 - Pan Malinowski - <a href="https://ipla-e2-18.pluscdn.pl/p/vm2movies/4q/4qaznvf7ky9n27jc8d1ibzv1gpb72bnp.mp4">link</a></li>
+  <li>104 - Relaks - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/gi/gio7dnn3bw8pdr1udxg72fyspc9k8n5o.mp4">link</a></li>
+  <li>105 - Podróżnik - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/6d/6dvkqwm4d7q8s6jhnw9szt9jqge7y6xn.mp4">link</a></li>
+  <li>106 - Strajk - <a href="https://ipla-e2-17.pluscdn.pl/p/vm2movies/xp/xp5d5r49faygktyq9n4mxs6r8m7a3ytd.mp4">link</a></li>
+  <li>107 - Kolęda - <a href="https://ipla-e1-79.pluscdn.pl/p/vm2movies/mb/mb4uvz5qz5j1emwh3ytqpiw5zfo5o4s2.mp4">link</a></li>
+  <li>108 - Pielgrzymka - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/kw/kw9p3c7woj7sc9g5rtgrz5o7v9ixy48y.mp4">link</a></li>
+  <li>109 - Karp - <a href="https://ipla-e1-83.pluscdn.pl/p/vm2movies/4h/4h5ms6sny5e1k7qjqbxd8r71u6eb99np.mp4">link</a></li>
+  <li>110 - Telewizor - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/aa/aau2v9m13bxznngy3b3nzp8b2m4uzj8m.mp4">link</a></li>
+</ul>
+
+<h3>SEZON 9</h3>
+<ul>
+  <li>111 - Wyjazd integracyjny - <a href="https://ipla-e2-17.pluscdn.pl/p/vm2movies/uv/uvxqgk4c8ogt41h7ft38xq2kz91yfxwc.mp4">link</a></li>
+  <li>112 - Egzamin - <a href="https://ipla-e1-77.pluscdn.pl/p/vm2movies/zx/zxnyj2qjmrp7tw7q1hnj1e6jz3vnbch8.mp4">link</a></li>
+  <li>113 - Policjant - <a href="https://ipla-e1-80.pluscdn.pl/p/vm2movies/tf/tfzv8qegk1muw7y73zj7hr8hccdr92ax.mp4">link</a></li>
+  <li>114 - Seksowny sąsiad - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/fe/fep1d9c8h2c9m5e5q9oqfkg66v1m9z1k.mp4">link</a></li>
+  <li>115 - Ryba - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/p2/p2a5sh84mnd5iv7r7ywmk91y5h3t6sh8.mp4">link</a></li>
+  <li>116 - Okulary - <a href="https://ipla-e2-17.pluscdn.pl/p/vm2movies/rm/rmfebqg5usn5ht3csx8d4yo3c4yp6b7f.mp4">link</a></li>
+  <li>117 - Żart - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/wi/wipryfbfjb1oeh8hmnkkddrv5z5ohrw9.mp4">link</a></li>
+  <li>118 - Zawał - <a href="https://ipla-e1-83.pluscdn.pl/p/vm2movies/dy/dy5p7y5rwnf6ec3y4odc3n3u8u3fsr8w.mp4">link</a></li>
+  <li>119 - Gwiazdor - <a href="https://ipla-e2-19.pluscdn.pl/p/vm2movies/jj/jjtnymnft6z5zrkpf7rkp8r2ko6h1t9p.mp4">link</a></li>
+  <li>120 - Gwiazda TV - <a href="https://ipla-e2-23.pluscdn.pl/p/vm2movies/ce/ce4hzqgqfn7jmw2uokj3g7xekx9k5q5o.mp4">link</a></li>
+  <li>121 - Karol zostaje dziadkiem - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/7s/7sgh7r7uzb1m8k84kfo8v9i1tgr7j9vb.mp4">link</a></li>
+  <li>122 - Świadek - <a href="https://ipla-e2-22.pluscdn.pl/p/vm2movies/hn/hnn9pfskpxx1my23u3c8of6iqbs9fjv7.mp4">link</a></li>
+  <li>123 - Złodziej - <a href="https://ipla-e2-20.pluscdn.pl/p/vm2movies/1g/1g7rwhf5r9x6z6b2yq8yovrbwtn6gx2q.mp4">link</a></li>
+  <li>124 - Przyszłość - <a href="https://ipla-e2-16.pluscdn.pl/p/vm2movies/gs/gsd9f8z2p7jz6m8s7g5b6u7h9n4m5x8c.mp4">link</a></li>
+  <li>125 - Agent - <a href="https://ipla-e2-18.pluscdn.pl/p/vm2movies/wz/wz4vh2sxg2m5px9b7m7r3z2k1s4xq8gh.mp4">link</a></li>
+  <li>126 - Pranie - <a href="https://ipla-e2-19.pluscdn.pl/p/vm2movies/6v/6v5m9e7p4g6y3j8s2n4q7c1b9h6k2x7r.mp4">link</a></li>
+  <li>127 - Przeznaczenie - <a href="https://ipla-e1-82.pluscdn.pl/p/vm2movies/hs/hs8y3x9v1d4t7j2g6m9q5n4b2r7w1c3d.mp4">link</a></li>
+  <li>128 - Poród - <a href="https://ipla-e1-83.pluscdn.pl/p/vm2movies/qm/qm6z7c2d9p4">Link</a></li>
+</ul>
+
+</div>
+</div>    )
+}
+
+export default miodowelata
--- src/pages/siecikomputerowe/odcinki/odc3.jsx
+++ src/pages/siecikomputerowe/odcinki/odc3.jsx
@@ -7,9 +7,21 @@ const odc3 = () => {
         <img className="itachi" src="/images/itachi-uchiha-naruto-amoled-black-background-minimal-art-3840x2160-6478.jpg"/>
         <iframe width="853" height="480" src="https://www.youtube.com/embed/eb_2O3STPsI"></iframe>
         <h1>Sieci komputerowe odc. 3 - Warstwa aplikacji</h1>
-        <ul>
-            <li></li>
-        </ul>
+<img className="itachi" src="/images/fido1.png"/>
+<img className="itachi" src="/images/fido2.png"/>
+<img className="itachi" src="/images/fido3.png"/>
+<img className="itachi" src="/images/fido4.png"/>
+<img className="itachi" src="/images/fido5.png"/>
+<img className="itachi" src="/images/fido6.png"/>
+<img className="itachi" src="/images/fido7.png"/>
+<img className="itachi" src="/images/fido8.png"/>
+<img className="itachi" src="/images/fido9.png"/>
+<img className="itachi" src="/images/fido10.png"/>
+<img className="itachi" src="/images/fido11.png"/>
+<img className="itachi" src="/images/fido12.png"/>
+<img className="itachi" src="/images/fido13.png"/>
+
+
     </div>
 </div>    
 )
