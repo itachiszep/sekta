0526d32d2740c89771d4627ee36d9615b14e3643
itachiszep <itachiszep@int.pl>
2025-09-07 19:15:22 +0200

    added miodowelata

--- src/pages/siecikomputerowe/odcinki/odc3.jsx
+++ src/pages/siecikomputerowe/odcinki/odc3.jsx
@@ -20,6 +20,27 @@ const odc3 = () => {
 <img className="itachi" src="/images/fido11.png"/>
 <img className="itachi" src="/images/fido12.png"/>
 <img className="itachi" src="/images/fido13.png"/>
+<img className="itachi" src="/images/fido14.png"/>
+<img className="itachi" src="/images/fido15.png"/>
+<img className="itachi" src="/images/fido16.png"/>
+<img className="itachi" src="/images/fido17.png"/>
+<img className="itachi" src="/images/fido18.png"/>
+<img className="itachi" src="/images/fido19.png"/>
+<img className="itachi" src="/images/fido20.png"/>
+<img className="itachi" src="/images/fido21.png"/>
+<img className="itachi" src="/images/fido22.png"/>
+<img className="itachi" src="/images/fido23.png"/>
+<img className="itachi" src="/images/fido24.png"/>
+<img className="itachi" src="/images/fido25.png"/>
+<img className="itachi" src="/images/fido26.png"/>
+<img className="itachi" src="/images/fido27.png"/>
+<img className="itachi" src="/images/fido28.png"/>
+<img className="itachi" src="/images/fido29.png"/>
+<img className="itachi" src="/images/fido30.png"/>
+<img className="itachi" src="/images/fido31.png"/>
+<img className="itachi" src="/images/fido32.png"/>
+<img className="itachi" src="/images/fido33.png"/>
+
 
 
     </div>
