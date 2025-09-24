3fedc10cb48b02b4af9567bb4122589a3333ab11
itachiszep <itachiszep@int.pl>
2025-09-24 20:14:40 +0200

    opis zmian

--- src/pages/kalilinux/odcinki/dziendobrykali.jsx
+++ src/pages/kalilinux/odcinki/dziendobrykali.jsx
@@ -6,7 +6,7 @@ const dziendobrykali = () => {
       <div className='center'>
         <img className="itachi" src="/images/itachi-uchiha-naruto-amoled-black-background-minimal-art-3840x2160-6478.jpg"/>
         <iframe width="853" height="480" src="https://www.youtube.com/embed/_M45c8jClWI"></iframe>    
-        <iframe width="853" height="480" src="https://www.youtube.com/embed/7RUylQDHQMk"></iframe>
+        
 
           </div>
 </div>  
