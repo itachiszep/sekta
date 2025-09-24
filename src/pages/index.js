21d785c4478dac11ed2572eff0f0fe0aead28c58
itachiszep <itachiszep@int.pl>
2025-09-24 19:51:13 +0200

    opis zmian

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ export default function Home() {
     <div>
       <div className='center'>
         <img className="itachi" src="/images/itachi-uchiha-naruto-amoled-black-background-minimal-art-3840x2160-6478.jpg"/>
+        <p>Itachi </p>
         <iframe width="853" height="480" src="https://www.youtube.com/embed/7RUylQDHQMk"></iframe>
+
       </div>
     </div>
   );
